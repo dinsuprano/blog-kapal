5803b14defa63baed1ffb98ee53451294d481359
dinsuprano <dinsuprano@gmail.com>
2025-07-28 15:48:06 +0800

    update-2.0

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            // ->registration()
+            ->registration()
             ->profile()
             ->colors([
                 'primary' => Color::Amber,

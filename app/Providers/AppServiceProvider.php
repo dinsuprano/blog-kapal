bd843241b62ab792d5c24f522c73f9daf12d28b0
dinsuprano <dinsuprano@gmail.com>
2025-07-31 15:25:29 +0800

    update-2.7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-    }
+        
+        // Serve files manually if symlink is not available
+        \Route::get('/storage/{path}', function ($path) {
+            $file = storage_path('app/public/' . $path);
+
+            if (!file_exists($file)) {
+                abort(404);
+            }
+
+            return Response::file($file);
+        })->where('path', '.*');
+        }
 }
--- copy-image.php
+++ copy-image.php
@@ -1,3 +1,24 @@
 <?php
-exec('cp -r storage/app/public/* public/storage/');
-?>
+
+function copyFiles($src, $dst) {
+    $dir = opendir($src);
+    @mkdir($dst);
+
+    while(false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src . '/' . $file)) {
+                copyFiles($src . '/' . $file, $dst . '/' . $file);
+            } else {
+                copy($src . '/' . $file, $dst . '/' . $file);
+            }
+        }
+    }
+    closedir($dir);
+}
+
+$source = '/home/u883355420/domains/airworthinessforum.com/public_html/storage/app/public';
+$destination = '/home/u883355420/domains/airworthinessforum.com/public_html/public/storage';
+
+copyFiles($source, $destination);
+
+echo "Files copied successfully!";
